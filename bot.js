c266e66a1de90dd9bd2d833b1f1752f65ea80f70
CryptoNiche <128776086+CryptoNiche@users.noreply.github.com>
2026-01-26 02:21:59 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const SHEET_ID = process.env.SHEET_ID;
 const PREFIXES = ["goodluck", "#gatetreadfiii", "#quiz123", "#Dj123"];
 
 // 🔐 OWNER LOCK
-const OWNER_ID = 933749968;
+const OWNER_ID = 933749968,8179916179;
 
 // system sheet name
 const ENABLED_SHEET = "__enabled_groups__";
